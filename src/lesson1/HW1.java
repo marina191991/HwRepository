b0e44a874e9ed4dc66434e7e8af970f00b9759bc
Marina <martiniz1901@gmail.com>
2021-09-22 00:23:56 +0300

    ДЗ готово

---
+++ src/lesson1/HW1.java
@@ -0,0 +1,58 @@
+package ru.geekbrains.homework1;
+
+public class HomeWorkApp {
+   public static void main (String[] args)
+   { Fruits();
+      System.out.println("****");
+ int z = 0;
+   checkSumSign(5, 10);
+      if (z>=0) {
+         System.out.println("Сумма положительная");
+      }
+      else {System.out.println("Сумма отрицательная");
+      }
+       System.out.println("****");
+
+       printColor(20);
+       System.out.println("****");
+       compareNumbers(44,82);
+
+   }
+
+   //ex.2
+   static void Fruits() {
+   System.out.println("Orange");
+   System.out.println("Banana");
+   System.out.println("Apple");
+   }
+
+   //ex.3
+   static void checkSumSign(int a, int b) {
+      int z = a + b;
+   }
+
+   // ex.4
+    static void printColor(int value) {
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    // ex.5
+    static void compareNumbers(int a,int b)
+    {
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else
+            { System.out.println("a < b");}
+    }
+
+    }
+
+
